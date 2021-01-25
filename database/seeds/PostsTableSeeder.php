95e140dbfffe3dcf43827192e216bad861c6b6ed
Francesco <fraancesco.bello@gmail.com>
2021-01-25 17:53:58 +0100

    Aggiunti dati Fake

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use app\post;
+use App\Post;
 use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
@@ -13,12 +13,12 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i < 20 ; $i++) {
-          $new_post= new post();
-          $new_post->username = $faker->userName();
-          $new_post->title = $faker->sentence();
-          $new_post->description = $faker->paragraphs();
-          $new_post->post_date = $faker->date();
+        for ($i=0; $i<20; $i++) {
+          $new_post = new Post();
+          $new_post->username= $faker->userName();
+          $new_post->title= $faker->sentence(2);
+          $new_post->description= $faker->paragraphs(2, true);
+          $new_post->post_date= $faker->date();
           $new_post->save();
         }
     }
